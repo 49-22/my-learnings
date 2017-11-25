3d4d551f9cb0fd9eec36cd697b73901b52f0a149
Nitin Nagamalla <nnagamalla@innominds.com>
2017-11-25 16:47:48 +0530

    Added Datepicker maxDate
    Added broken vuetify through dependency in package.json and added max date as today to chose.

--- src/components/datePicker.vue
+++ src/components/datePicker.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-menu>
       <v-text-field slot="activator" label="Pick a date to view" v-model="picker" prepend-icon="event" readonly></v-text-field>
-      <v-date-picker v-model="picker" :date-format="date => new Date(date).toDateString()" :formatted-value.sync="formatted"></v-date-picker>
+      <v-date-picker v-model="picker" :date-format="date => new Date(date).toDateString()" :formatted-value.sync="formatted" :allowed-dates="allowedDates"></v-date-picker>
     </v-menu>
   </v-container>
 </template>
@@ -12,8 +12,12 @@ export default {
   data () {
     return {
       picker: null,
-      formatted: null
+      formatted: null,
+      allowedDates: null
     }
+  },
+  mounted () {
+    this.allowedDates.max = new Date();
   }
 }
 </script>
